3b89b2e19933e73de0a397bb47155c8aafa56b5f
Moritz Herzog <m_herzog15@cs.uni-kl.de>
2023-05-15 13:29:36 +0200

    Added Working Json Parser Package

--- realtwester/conanfile.py
+++ realtwester/conanfile.py
@@ -23,6 +23,7 @@ class digitwesterRecipe(ConanFile):
     def requirements(self):
         self.requires("openssl/1.1.1t")
         self.requires("libcurl/7.79.1")
+        self.requires("nlohmann_json/3.11.2")
     
     def layout(self):
         cmake_layout(self)
