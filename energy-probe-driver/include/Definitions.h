af2b1b3d5c37217b5596222d479df628c0e270d3
Moritz Herzog <m_herzog15@cs.uni-kl.de>
2023-05-19 17:22:25 +0200

    Adding Classes for driver. Need to find an alternative for the SessionManagement within the driver

---
+++ energy-probe-driver/include/Definitions.h
@@ -0,0 +1,28 @@
+//
+// Created by moritzh on 19.05.23.
+//
+
+#ifndef ENERGYPROBEDRIVER_DEFINITIONS_H
+#define ENERGYPROBEDRIVER_DEFINITIONS_H
+
+#define MAX_EPROBE_CHANNELS     3
+#define MAX_DAQ_CHANNELS        40  // A caiman 'channel' includes V+I
+
+#define MAX_CHANNELS MAX_DAQ_CHANNELS
+#define MAX_FIELDS_PER_CHANNEL 3
+#define MAX_FIELDS MAX_CHANNELS * MAX_FIELDS_PER_CHANNEL
+#define MAX_COUNTERS MAX_FIELDS * 2 // one for peak, one for average
+#define MAX_STRING_LEN 80
+#define MAX_DESCRIPTION_LEN 400
+
+// Fields
+static const char * const field_title_names[] = { "", "Power", "Voltage", "", "Current" };
+enum
+{
+    POWER = 1,
+    VOLTAGE = 2,
+    CURRENT = 4
+};
+
+
+#endif //ENERGYPROBEDRIVER_DEFINITIONS_H
