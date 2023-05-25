5a1fc69a6e878d344e6ff91973caebb207b0b9b8
Moritz Herzog <m_herzog15@cs.uni-kl.de>
2023-05-25 11:19:37 +0200

    Updte to new Project Properties

--- realtwester/src/RealTwester.h
+++ realtwester/src/RealTwester.h
@@ -5,7 +5,7 @@
 #include <include/DriverSessionManager.h>
 
 namespace REALTWESTER {
-    class DIGITWESTER_EXPORT RealTwester {
+    class RealTwester {
     public:
         RealTwester();
         virtual ~RealTwester();
