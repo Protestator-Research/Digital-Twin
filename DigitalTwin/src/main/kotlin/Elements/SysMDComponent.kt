044c01597f0786b354f6be103ad0d9fffac30098
Moritz Herzog <m_herzog15@cs.uni-kl.de>
2023-06-28 16:37:47 +0200

    Fixing Issues

--- DigitalTwin/src/main/kotlin/Elements/SysMDComponent.kt
+++ DigitalTwin/src/main/kotlin/Elements/SysMDComponent.kt
@@ -1,4 +1,12 @@
 package Elements
 
+
+
 class SysMDComponent : SysMDElement {
+    override fun evaluateConnection() {
+        TODO("Not yet implemented")
+    }
+
+    override val connectionType: ConnectionType
+        get() = TODO("Not yet implemented")
 }
