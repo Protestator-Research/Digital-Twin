7a48bc314fdc26bc97e6932a7f299e3c35e2e65c
Moritz Herzog <m_herzog15@cs.uni-kl.de>
2024-02-27 13:06:28 +0100

    Adding Documentation for all Record fuctions and elements.

--- CppDigitalTwin/CpsBaseLib/src/SysMLv2Standard/entities/Record.h
+++ CppDigitalTwin/CpsBaseLib/src/SysMLv2Standard/entities/Record.h
@@ -65,53 +65,77 @@ namespace SysMLv2::Entities {
         bool operator==(Record const &other);
 
         /**
-         *
-         * @return
+         * Returns the human identifier according to the SysMLv2 standard.
+         * @return Human Identifier.
+         * @see std::string
          */
         [[nodiscard]] std::string getName() const;
 
         /**
-         *
-         * @return
+         * Returns the list of all Aliases according ot the SysML Standard. Hereby also holds that the name aka. the
+         * human identifier is part of the alias list.
+         * @return All available Aliases.
+         * @see std::list
+         * @see std::string
          */
         [[nodiscard]] std::list<std::string> getAlias() const;
 
         /**
-         *
-         * @param alias
+         * Appends an single alias to the aliases list of a single Record.
+         * @param alias Additional alias, that will be stored in tha alias list.
          */
         void appendAlias(std::string& alias);
 
         /**
-         *
-         * @return
+         * Returns the description of a Record according to the SysMLv2 standard.
+         * @return A std::string containing the Description of the Record.
          */
         [[nodiscard]] std::string getDescription() const;
 
         /**
-         *
-         * @param description
+         * Sets a new description of the Record.
+         * @param description The description is given as a std::string.
          */
         void setDescription(std::string& description);
 
         /**
-         *
-         * @return
+         * Returns the ID of the Record. The ID is only gettable and not settable except the option to set the ID
+         * in the constructor according to the SysMLv2 Standard.
+         * @return A boost::uuids::uuid, as the ID.
+         * @see boost::uuids::uuid
          */
         [[nodiscard]] boost::uuids::uuid getId() const;
 
         /**
-         *
-         * @return
+         * Returns the type of the Record. The type is a complexity that is needed to parse an object. Also there is no
+         * point in making the type settable, since the type is dependent on the subclass of the Record that is used.
+         * @return String representation of the Type.
          */
         [[nodiscard]] std::string getType() const;
 
         std::string serializeToJson() override;
     protected:
+        /**
+         * Type of the generated class. This also enables the parsing of the element.
+         */
         std::string Type;
+        /**
+         * Contains the name of the Record Class. This is also called human identifier within the SysMLv2 Documentation.
+         */
         std::string Name;
+        /**
+         * Contains all Aliases of the object. Also within the Aliases the human identifier needs to be contained,
+         * according to the SysMLv2 standard.
+         */
         std::list<std::string> Alias;
+        /**
+         * Contains the description of a single Record.
+         */
         std::string Description;
+        /**
+         * Contains the ID of a single Record. This is also stored as a UUID.
+         * @see boost::uuids::uuid
+         */
         boost::uuids::uuid Id;
     };
 }
