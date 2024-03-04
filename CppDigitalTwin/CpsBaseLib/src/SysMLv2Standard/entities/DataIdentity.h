42b11d6f28fdb6accb609ca8f9fdadbea10e76e4
Moritz Herzog <m_herzog15@cs.uni-kl.de>
2024-03-04 10:43:34 +0100

    Fixing build

--- CppDigitalTwin/CpsBaseLib/src/SysMLv2Standard/entities/DataIdentity.h
+++ CppDigitalTwin/CpsBaseLib/src/SysMLv2Standard/entities/DataIdentity.h
@@ -11,6 +11,7 @@
 //---------------------------------------------------------
 #include <boost/uuid/uuid.hpp>
 #include <vector>
+#include <chrono>
 
 //---------------------------------------------------------
 // Internal Classes
