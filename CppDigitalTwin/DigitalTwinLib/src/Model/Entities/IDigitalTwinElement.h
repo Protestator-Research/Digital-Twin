588d1728a73096f0ed6b59806fda0ae14e4a5f13
TheMHMoritz3 <TheMHMoritz3@users.noreply.github.com>
2025-01-24 09:42:10 +0100

    working on thread protected paper

--- CppDigitalTwin/DigitalTwinLib/src/Model/Entities/IDigitalTwinElement.h
+++ CppDigitalTwin/DigitalTwinLib/src/Model/Entities/IDigitalTwinElement.h
@@ -9,11 +9,26 @@
 #include "../../cpp_digital_twin_lib_global.h"
 
 namespace DigitalTwin::Model {
+    /**
+     * @class IDigitalTwinElement
+     * @author Moritz Herzog
+     * @version 1.0
+     * This Class represents the BaseClass of the Elements of the Digital Twin.
+     * If there are parts that are needed across all DT Elements, this holds it.
+     */
     class CPPDIGITALTWINLIB_EXPORT IDigitalTwinElement {
     public:
+        /**
+         * Is deleted, because there is no need.
+         */
         IDigitalTwinElement() = delete;
 
-        IDigitalTwinElement(std::string name){
+        /**
+         * Constructor that initializes the name of the Element. The name can never be changed,
+         * because it is a core component in the
+         * @param name Name of a DT Element
+         */
+        explicit IDigitalTwinElement(std::string name){
             Name = name;
         }
 
